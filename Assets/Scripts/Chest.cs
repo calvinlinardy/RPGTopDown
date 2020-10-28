6b308a1f8fe46f343710558cbdd9b186b2c7f120
calvinlinardy <calvin.linardy@yahoo.com>
2020-10-28 22:20:59 +0800

    More Updates
    - added lobby.
    - added first dungeon.
    - added tiles and tilemaps.
    - added chest and portal.

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : Collectable
+{
+    [SerializeField] Sprite emptyChestSprite = null;
+    [SerializeField] int pesosAmount = 10;
+    protected override void OnCollect()
+    {
+        if (!collected)
+        {
+            collected = true;
+            GetComponent<SpriteRenderer>().sprite = emptyChestSprite;
+            Debug.Log("Grant " + pesosAmount + " pesos!");
+        }
+    }
+}
---
+++ Assets/Scripts/Collidable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collidable : MonoBehaviour
+{
+    BoxCollider2D boxCollider;
+    ContactFilter2D filter = default;
+    Collider2D[] hits = new Collider2D[10];
+    
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        boxCollider.OverlapCollider(filter, hits);
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (hits[i] == null)
+                continue;
+                OnCollide(hits[i]);
+                hits[i] = null;
+        }
+    }
+
+    protected virtual void OnCollide(Collider2D coll)
+    {
+        Debug.Log(coll.name);
+    }
+}
