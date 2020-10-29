378df17b047cf9da93b304824fc1853fe6b169b9
calvinlinardy <calvin.linardy@yahoo.com>
2020-10-29 15:36:30 +0800

    Added Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private void Awake()
+    {
+        instance = this;
+        SceneManager.sceneLoaded += LoadState;
+    }
+
+    // Resources
+    public List<Sprite> playerSprites;
+    public List<Sprite> weaponSprites;
+    public List<int> weaponPrices;
+    public List<int> xpTable;
+
+    //Refrences
+    public Player player;
+
+    // Logic
+    public int pesos;
+    public int experience;
+    
+    public void SaveState()
+    {
+        Debug.Log("Save State");
+    }
+
+    public void LoadState(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("Load State");   
+    }
+}
